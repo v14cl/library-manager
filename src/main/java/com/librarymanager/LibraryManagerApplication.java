fe489a651291bce1e75c2ebccfa120036942ed31
v14cl <vlmakotkin@gmail.com>
2025-11-25 14:37:07 +0200

    Changed main project class to LibraryManagerApplication.java

---
+++ src/main/java/com/librarymanager/LibraryManagerApplication.java
@@ -0,0 +1,10 @@
+package com.librarymanager;
+
+import java.sql.SQLException;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerApplication {
+    public static void main(String[] args) throws SQLException {
+    }
+}
