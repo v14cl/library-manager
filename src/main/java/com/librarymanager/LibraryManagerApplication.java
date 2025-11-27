db319f168b0862afb4b747aed77e9313601b61ad
v14cl <vlmakotkin@gmail.com>
2025-11-27 11:04:41 +0200

    Add SpringApplication.run()

--- src/main/java/com/librarymanager/LibraryManagerApplication.java
+++ src/main/java/com/librarymanager/LibraryManagerApplication.java
@@ -1,10 +1,13 @@
 package com.librarymanager;
 
 import java.sql.SQLException;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryManagerApplication {
     public static void main(String[] args) throws SQLException {
+        SpringApplication.run(LibraryManagerApplication.class, args);
     }
 }
